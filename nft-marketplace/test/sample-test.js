7cd5c8cb903470836e97de0ed2ad19471f18f113
pampakid <pampakidx@gmail.com>
2022-01-05 15:11:05 -0300

    Update sample-test.js for testing out contracts

--- nft-marketplace/test/sample-test.js
+++ nft-marketplace/test/sample-test.js
@@ -1,19 +1,52 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+/* Test contracts with key functionalities like minting a token, 
+  putting it up for sale,   selling it to a user, and querying for tokens. */
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("NFTMarket", function () {
+  it("Should create and execute market sales", async function () {
+    // Create test that simulated deploying both contracts, create an NFT, put it on sale and purchase it from someone else 
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    // Get a reference to the market contract
+    const Market = await ethers.getContractFactory("NFTMarket")
+    const market = await Market.deploy() // deploy the market
+    await market.deployed() // wait for it to finish being deployed   
+    // Get a reference to the address to which it was deployed (we need the value of the market address)
+    const marketAddress = market.address
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    // Deploy the NFT contract
+    const NFT = await ethers.getContractFactory("NFT")
+    const nft = await NFT.deploy(marketAddress) // pass in the market address to the constructor of the NFT
+    await nft.deployed()
+    // Reference to the NFT contract itself
+    const nftContractAddress = nft.address
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    // Interact with the contracts already deployed
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});
+    // Get reference to the value of the listing price
+    let listingPrice = await market.getListingPrice()
+    listingPrice = listingPrice.toString() // turn it into a string
+    
+    // Value for how much we are selling the item for
+    const auctionPrice = ethers.utils.parseUnits('1', 'ether') // use ether utils to work with whole units instead of decimals (in our case Matic has 18 decimal places)
+
+    // Create tokens interacting with the NFT contract
+    await nft.createToken("https://www.mytokenlocation.com")
+    await nft.createToken("https://www.mytokenlocation2.com")
+
+    await market.createMarketItem(nftContractAddress, 1, auctionPrice, { value: listingPrice })
+    await market.createMarketItem(nftContractAddress, 2, auctionPrice, { value: listingPrice })
+
+    // Set up test accounts using the ethers library
+    // We ignore the first address using an "_" cause it's gonna be used when deploying
+    // We get a reference to the buyer address because we don't want the buyer to be the same as the seller ("_") 
+    const [_, buyerAddress] = await ethers.getSigners()
+    // Use the buyerAddress to connect to the market and sell nft 1
+    await market.connect(buyerAddress).createMarketSale(nftContractAddress, 1, { value: auctionPrice})
+
+    // Test querying for these market items
+    // Variable called items
+    items = await market.fetchMarketItems()
+
+    console.log('items: ', items)
+
+  })
+})
