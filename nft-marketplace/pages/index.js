ea8506120a26f21c026033582985bf97b846131d
pampakid <pampakidx@gmail.com>
2022-01-06 12:09:23 -0300

    Update indexjs and add fx loadNFTs

--- nft-marketplace/pages/index.js
+++ nft-marketplace/pages/index.js
@@ -1,9 +1,58 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { div } from 'prelude-ls'
-import styles from '../styles/Home.module.css'
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import { div } from 'prelude-ls'
+// import styles from '../styles/Home.module.css'
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react' // hooks. useState --> keep up with local states. useEffect --> invoke a function when the component loads
+import axios from 'axios' // data fetching library
+import Web3Modal from 'web3modal' // connect to someone's ethereum wallet
+
+/* Import references to addresses */
+import {
+  nftaddress, nftmarketaddress
+} from '../config.js'
+
+/* Import ABIs for the ether client to interact with the contracts*/
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
+import Market from '../artifacts/contracts/Market.sol/NFTMarket.json'
 
 export default function Home() {
+  const [nfts, setNfts] = useState([]) // empty array of nfts and a function to reset the array of nfts 
+  const [loadingState, setLoadingState] = useState('not-loaded') // loading state is a variable and the setLoadingState will let us update the variable. default not-loaded
+  
+  useEffect(() => {
+    loadNFTs()
+  }, [])
+
+  async function loadNFTs() {
+    const provider = new ethers.providers.JsonRpcProvider()
+    const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
+    const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider)
+    const data = await marketContract.fetchMarketItems()
+
+    // Map over the items
+    const items = await Promise.all(data.map(async i => {
+      const tokenUri = await tokenContract.tokenURI(i.tokenId)
+      const meta = await axios.get(tokenUri)
+      let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+      let item = {
+        price,
+        tokenId:      i.tokenId.toNumber(),
+        seller:       i.seller,
+        owner:        i.owner,
+        image:        meta.data.image,
+        name:         meta.data.name,
+        description:  meta.data.description
+      }
+      return item
+    }))
+    setNfts(items)
+    setLoadingState('loaded')
+  }
+  if (loadingState === 'loaded' && !nfts.length) return (
+    <h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>
+  )
+
   return (
     <div className={styles.container}>
       <h1>Home</h1>
