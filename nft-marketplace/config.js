f5f384cb1ca729508a0ea7c8d7c059d21ae6c55e
pampakid <pampakidx@gmail.com>
2022-01-06 14:47:47 -0300

    Update addresses on configjs

--- nft-marketplace/config.js
+++ nft-marketplace/config.js
@@ -1,2 +1,3 @@
-export const nftaddress = ""
-export const nftmarketaddress = ""
+/* When we deploy our project we need to have a reference to our addresses */
+export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
