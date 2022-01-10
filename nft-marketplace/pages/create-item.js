1061e16a5fac01c264c6c504bd3b1167fa7f7d6e
pampakid <pampakidx@gmail.com>
2022-01-10 20:46:45 -0300

    Add createItem and createSale functions

---
+++ nft-marketplace/pages/create-item.js
@@ -0,0 +1,95 @@
+import { useState } from 'react'
+import { ethers } from 'ethers'
+import { create as ipfsHttpClient } from 'ipfs-http-client'
+import { useRouter } from 'next/router'
+import Web3Modal from 'web3modal'
+
+/* Create variable called client to set and pin items to ipfs */
+const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+// Import addresses
+import {
+    nftaddress, nftmarketaddress
+} from '../config.js'
+
+// Import ABIs
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
+import Market from '../artifacts/contracts/Market.sol/NFTMarket.json'
+import createStatsCollector from 'mocha/lib/stats-collector'
+
+// Define default export 
+export default function CreateItem () {
+    const [fileUrl, setFileUrl] = useState(null) // ipfs file we are going to allow the user to upload
+    const[formInput, updateFormInput] = useState({ price: '', name: '', description: '' }) // allow user to set the price, name and desc of the nft
+    const router = useRouter() 
+
+    // Get url of NFT once created
+    async function onChange(e) {
+        const file = e.target.files[0]
+        try {
+            const added = await client.add(
+                file,
+                {
+                    progress: (prog) => console.log( `received: ${prog}` )
+                }
+            )
+            const url = `https://ipfs.infura.io/ipfs/${added.path}`
+            setFileUrl(url)
+        } catch (e) {
+            console.log('Error uploading file: ', e)
+        }
+    }
+
+    /* Create function to allow user to list an item for sale */
+    /* We are going to break it up on two functions for that: 
+            1. Creating an item and saving it to ipfs (reference to the NFT)
+            2. Listing the item for sale */
+    async function createItem() {
+        const { name, description, price } = formInput // destructure the name description and price
+        // First form of validation, we don't want to let users list items that don't have this available
+        if (!name || !description || !price || !fileUrl) return 
+        // Save value to IPFS in variable called data
+        const data = JSON.stringify({
+            name, description, image: fileUrl
+        })
+        // Save data to IPFS using a try&catch
+        try {
+            const added = await client.add(data)
+            const url = `https://ipfs.infura.io/ipfs/${added.path}`
+            /* after file is uploaded to IPFS hte URL to save it on Polygon */ 
+            createSale(url)
+        } catch (error) {
+            console.log('Error uploading file: ', error)
+        }
+    } 
+
+    async function createSale() {
+        const web3Modal = new Web3Modal()
+        const connection = await web3Modal.connect() // connect
+        const provider = new ethers.providers.Web3Provider(connection) // get provider
+        const signer = provider.getSigner() // get signer
+
+        /* We are going to be interacting with 2 contracts */
+        let contract = new ethers.Contract(nftaddress, NFT.abi, signer) // nft contract
+        let transaction = await contract.createToken(url) // create token
+        let tx = await transaction.wait() // wait for transaction to succeed
+        /* Modify the events array we get back to use its values */
+        let event = tx.events[0] // reference to the events array
+        let value = event.args[2] // get the third value out of args
+        let tokenId = value.toNumber() // turn that value into a number (rn its a BigNumber) to create the reference to the token id
+
+        /* Get reference to the price we want to sell the item for */
+        const price = ethers.utils.parseUnits(formInput.price, 'ether')
+        /* Move the reference of the contract from the nft to the nft market address and market contract  */
+        contract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
+        let listingPrice = await contract.getListingPrice()
+        listingPrice = listingPrice.toString() // send the value within this transaction to the contract
+
+        transaction = await contract.createMarketItem( // wait contract.createMarketItem to succeed
+            nftaddress, tokenId, price, { value: listingPrice } // bring data
+        )
+        await transaction.wait()
+
+        /* Reroute user to anothe page */
+        router.push('/')
+    }
+} 
