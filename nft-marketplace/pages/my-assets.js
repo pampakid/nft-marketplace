a452454d804255ce40c87edd7b0954d89a31e339
pampakid <pampakidx@gmail.com>
2022-01-11 20:35:41 -0300

    Return UI to my-assetsjs

--- nft-marketplace/pages/my-assets.js
+++ nft-marketplace/pages/my-assets.js
@@ -49,4 +49,25 @@ export default function MyAssets() {
         setNfts(items)
         setLoadingState('loaded')
     }
+    if (loadingState === 'loaded' && !nftaddress.length) return (<h1 className="py-10 px-20 text-3xl">No assets owned</h1>)
+    return (
+        <div className="flex justyfi-center">
+            <div className="p-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+                    {
+                        nftaddress.map((nft, i) => (
+                            <div key={i} className="border shadow rounded-xl overflow-hidden">
+                                <img src={nft.image} className="rounded"/>
+                                <div className="p-4 bg-black">
+                                    <p className="text-2xl font-bold text-white">
+                                        Price - {nft.price} Eth
+                                    </p>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    )
 }
